ad2310e87f9d9c5e6fb78d8c95ea96d624d9b739
Jhudiel Van Muñoz <jhudzkymunoz@gmail.com>
2024-05-13 08:48:35 +0800

    adds tasks in tasks url

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -2,7 +2,7 @@ from rest_framework import routers
 from tasks import views
 
 router = routers.SimpleRouter()
-router.register(r"", views.TaskViewSet)
+router.register(r"tasks", views.TaskViewSet)
 router.register(r"initiatives", views.InitiativeViewSet)
 router.register(r"roadblocks", views.RoadblockViewSet)
 
