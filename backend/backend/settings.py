1196ee062c75c420334f35f283765f094c50bc54
Jethro Baring <baring.jeth@gmail.com>
2024-09-15 17:18:46 +0800

    Updated settings.py for deployment

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -138,6 +138,7 @@ SIMPLE_JWT = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",  # Example: your frontend URL
+    "https://chumcheck.vercel.app"
 ]
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
