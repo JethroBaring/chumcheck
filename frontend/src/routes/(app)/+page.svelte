a369b7aef3f895639f55908b62a926d415f012db
Jethro Baring <baring.jeth@gmail.com>
2024-09-15 16:22:12 +0800

    testing environment variable

--- frontend/src/routes/(app)/+page.svelte
+++ frontend/src/routes/(app)/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { PUBLIC_API_URL } from '$env/static/public';
 	import { Header, Hero, HowItWork, AboutUs, Footer } from '$lib/components/app';
 </script>
 
@@ -8,6 +9,7 @@
 
 <div class="bg-grid">
 	<div class="mx-auto w-full">
+		{PUBLIC_API_URL}
 		<Header />
 		<Hero />
 		<HowItWork />
