add20a861a52e9a4090c5134221526ef2a15e212
Jethro Baring <baring.jeth@gmail.com>
2024-09-20 20:54:26 +0800

    test

---
+++ frontendv2/src/lib/stores/useManagerRatedStore.svelte.ts
@@ -0,0 +1,43 @@
+export const useManagerRatedStore = (applicantsData: unknown[]) => {
+	const applicants = $state(applicantsData);
+	let showApplicantCapsuleProposal = $state(false);
+	let showPendingDialog = $state(false);
+
+	const toggleShowPendingDialog = () => {
+		showPendingDialog = !showPendingDialog;
+	};
+
+	const toggleShowApplicantCapsuleProposal = () => {
+		showApplicantCapsuleProposal = !showApplicantCapsuleProposal;
+	};
+
+  const getApplicantInformation = async (startupId: number) => {
+    console.log(startupId)
+  }
+
+  const updateApplicantScore = async (id: number, newScore: number) => {
+    console.log(id);
+    console.log(newScore)
+  }
+
+  const saveApplicantRating = async (startupId: number) => {
+    console.log(startupId)
+  }
+
+  return {
+    get applicants() {
+      return applicants;
+    },
+    get showApplicantCapsuleProposal() {
+      return showApplicantCapsuleProposal;
+    },
+    get showPendingDialog() {
+      return showPendingDialog;
+    },
+    toggleShowPendingDialog,
+    toggleShowApplicantCapsuleProposal,
+    getApplicantInformation,
+    updateApplicantScore,
+    saveApplicantRating
+  }
+};
---
+++ frontendv2/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+  import { runesCounter } from "$lib/stores/counter.svelte";
+</script>
+
+<h1>{runesCounter.count}</h1>
+<button onclick={runesCounter.increment}>+</button>
+
+<a href="/test">test</a>
