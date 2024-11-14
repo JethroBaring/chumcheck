9b20ea87c56a50fa4bafb1272036f9f5ccfff346
Jethro Baring <80811809+JethroBaring@users.noreply.github.com>
2024-11-14 17:35:54 +0800

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["chumcheck-production.up.railway.app"]
+ALLOWED_HOSTS = ["api.chumcheck.jethdev.tech"]
 
 # Application definition
 
