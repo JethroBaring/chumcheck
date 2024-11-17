6e6906e0e81852fbfd70fb106e26472f8546072b
Jethro Baring <baring.jeth@gmail.com>
2024-11-17 20:35:21 +0800

    updated token to include basic information

--- backend/users/serializers/base.py
+++ backend/users/serializers/base.py
@@ -18,32 +18,12 @@ class UserBaseSerializer(serializers.ModelSerializer):
         ]
 
 
-class CustomTokenObtainPairSerializer(TokenObtainPairSerializer):
-    pass
-
-
-class CustomTokenRefreshSerializer(serializers.Serializer):
-    access = serializers.CharField(read_only=True)
-    refresh = serializers.CharField(read_only=True)
-    token_class = RefreshToken
-
-    def validate(self, attrs):
-        user_id = self.context.get("user_id")
-
-        try:
-            user_obj = users_models.BaseUser.objects.get(id=user_id)
-        except users_models.BaseUser.DoesNotExist as exc:
-            raise ValidationError(f"No user found in the request. {user_id}") from exc
-
-        new_refresh_token = self.token_class.for_user(user_obj)
-
-        data = {
-            "access": str(new_refresh_token.access_token),
-            "refresh": str(new_refresh_token),
-        }
-
-        return data
-
-
-class TokenObtainResponseSerializer(serializers.Serializer):
-    access_token = serializers.CharField()
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token["user_type"] = user.user_type
+        token["email"] = user.email
+        token["first_name"] = user.first_name
+        token["last_name"] = user.last_name
+        return token
